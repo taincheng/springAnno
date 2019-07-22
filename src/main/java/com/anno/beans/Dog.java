33bb788c5a7257909eea7b8528661a8f130271b8
Old_man <402503294@qq.com>
2019-07-22 22:56:13 +0800

    Revert "Spring 对 BeanPostProcessor的底层实现"
    This reverts commit 3d146f1

--- src/main/java/com/anno/beans/Dog.java
+++ src/main/java/com/anno/beans/Dog.java
@@ -1,8 +1,5 @@
 package com.anno.beans;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +7,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Component("dog")
-public class Dog implements ApplicationContextAware{
-
-    private ApplicationContext ac;
-
+public class Dog {
     public Dog(){
         System.out.println("dog ... constructor...");
     }
@@ -28,11 +22,5 @@ public class Dog implements ApplicationContextAware{
     @PreDestroy
     public void destory(){
         System.out.println("dog... @PreDestroy....");
-    }
-
-    //ApplicationContextAware 将IOC容器注入到组件中。
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.ac = applicationContext;
     }
 }
